a3b61246b495e9d46763a5415df94972aeb1fc9f
Zeynep Dicle <zeynep.diclee@hotmail.com>
2022-08-22 15:54:31 +0300

    update robotic

--- Robotics/TemassizAnahtar/TemassizAnahtar.ino
+++ Robotics/TemassizAnahtar/TemassizAnahtar.ino
@@ -1,8 +1,10 @@
 #include <Deneyap_HareketIsikRenkAlgilayiciMesafeOlcer.h>
 #include <Deneyap_5x7LedEkran.h>
+#include "RGB5050.h"
 
 APDS9960 Hareket;
 DotMatrix LEDMatris;
+RGB5050 RGB5050;
                         
 void setup() {
   Serial.begin(115200);
@@ -10,12 +12,16 @@ void setup() {
   LEDMatris.begin(0x0A);
   Hareket.enableProximity(true);
   Hareket.enableGesture(true);
+  RGB5050.begin();
+  RGB5050.setBrightness(10); 
 }
 
 void loop() {    
   uint8_t gesture = Hareket.readGesture();
   if (gesture == APDS9960_DOWN) {
     Serial.println("Asagi");
+    RGB5050.setLedColorData(0, 255, 0, 0);
+    RGB5050.show(); 
     for (int i = 0; i < 250; i++) {
         LEDMatris.dotrow1(0, 0, 1, 0, 0);
         LEDMatris.dotrow2(0, 0, 1, 0, 0);
@@ -27,6 +33,8 @@ void loop() {
     }
   }
   if (gesture == APDS9960_UP) {
+    RGB5050.setLedColorData(0, 255, 255, 0);
+    RGB5050.show(); 
     Serial.println("Yukari");
     for (int i = 0; i < 250; i++) {
         LEDMatris.dotrow1(0, 0, 1, 0, 0);
@@ -39,6 +47,8 @@ void loop() {
     }
   }
   if (gesture == APDS9960_LEFT) {
+    RGB5050.setLedColorData(0, 0, 255, 255);
+    RGB5050.show(); 
     Serial.println("Sol");
     for (int i = 0; i < 250; i++) {
         LEDMatris.dotrow1(0, 0, 1, 0, 0);
@@ -51,6 +61,8 @@ void loop() {
     }
   }
   if (gesture == APDS9960_RIGHT) {
+    RGB5050.setLedColorData(0, 255, 0, 255);
+    RGB5050.show(); 
     Serial.println("Sag");
     for (int i = 0; i < 250; i++) {
         LEDMatris.dotrow1(0, 0, 1, 0, 0);
